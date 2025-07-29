632a1cf091fc1732b85130ceae55cdf80a7bc245
d4-n1 <dscasares@gmail.com>
2025-07-29 23:15:37 +0200

    Deleted examples in weather.js

--- part2/countries/src/services/weather.js
+++ part2/countries/src/services/weather.js
@@ -1,6 +1,3 @@
-// Example of API call:
-// api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=72a88db804a2596b491c75fa29a869ce
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 import axios from 'axios';
 
 const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
