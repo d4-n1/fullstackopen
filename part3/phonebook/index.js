ba0a6259f874a151264afab367b6b4dc1bcda586
d4-n1 <dscasares@gmail.com>
2025-10-24 09:35:45 +0200

    Exercise 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -36,6 +36,18 @@ app.get('/api/info', (req, res) => {
   res.send(info);
 });
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((p) => p.id === id);
+
+  if (person) {
+    return res.json(person);
+  } else {
+    res.statusMessage = "There's no person with that id";
+    res.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
