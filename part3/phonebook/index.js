20f71e6d274151b8ef148cd306d71180cd733396
d4-n1 <dscasares@gmail.com>
2025-11-08 20:22:18 +0100

    Removed cors

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 
 app.use(express.json());
 app.use(express.static('dist'));
-app.use(cors());
+// app.use(cors());
 const morgan = require('morgan');
 
 morgan.token('body', function (req, res) {
